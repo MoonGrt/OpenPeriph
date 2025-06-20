a8fe8ddc1fcd2947df50d1378aac8c9fd8f38a67
MoonGrt <1561145394@qq.com>
2025-06-20 22:30:54 +0800

    perf: gpio(full)

--- src/main/scala/periph/gpio/apb3gpio.scala
+++ src/main/scala/periph/gpio/apb3gpio.scala
@@ -40,12 +40,35 @@ case class Apb3Gpio(
   // val BSRR = Reg(Bits(gpioWidth bits)) init (0)
   // val BSR = Reg(Bits(gpioWidth bits)) init (0)
   val LCKR = Reg(UInt(gpioWidth bits)) init (0)
+  val LCKK = Reg(Bool()) init(False)
 
   // --- APB 寄存器映射，使用 Apb3SlaveFactory ---
-  ctrl.readAndWrite(CRL, 0x00) // 0x00  CRL 配置寄存器
-  ctrl.readAndWrite(CRH, 0x04) // 0x04  CRH 配置寄存器
-  ctrl.read(IDR, 0x08) // 0x08  IDR 输入数据寄存器，只读
-  ctrl.readAndWrite(ODR, 0x0c) // 0x0C  ODR 输出数据寄存器
+  // CRL CRH
+  // 只有LCKR=0的位置允许写入新数据，其他保持旧值
+  ctrl.onWrite(0x00) {
+    val wr = io.apb.PWDATA.asUInt
+    val maskSegments = Vec(Bits(4 bits), 8)
+    for(i <- 0 until 8) {
+      maskSegments(i) := Mux(!LCKR(i), B"1111", B"0000")
+    }
+    val mask = maskSegments.asBits.asUInt
+    CRL := (CRL & ~mask) | (wr & mask)
+  }
+  ctrl.read(CRL, 0x00)
+  ctrl.onWrite(0x04) {
+    val wr = io.apb.PWDATA.asUInt
+    val maskSegments = Vec(UInt(4 bits), 8)
+    for(i <- 0 until 8) {
+      maskSegments(i) := Mux(!LCKR(i + 8), U"4'b1111", U"4'b0000")
+    }
+    val mask = maskSegments.asBits.asUInt
+    CRH := (CRH & ~mask) | (wr & mask)
+  }
+  ctrl.read(CRH, 0x04)
+  // IDR 输入数据寄存器，只读
+  ctrl.read(IDR, 0x08) // 0x08 IDR
+  // ODR 输出数据寄存器，可读写
+  ctrl.readAndWrite(ODR, 0x0c) // 0x0C ODR
   // BSRR和BRR 写操作对ODR的实时影响
   ctrl.onWrite(0x10) { // BSRR
     val bsrrData = io.apb.PWDATA
@@ -53,32 +76,28 @@ case class Apb3Gpio(
     val br = bsrrData(gpioWidth * 2 - 1 downto gpioWidth).asUInt
     ODR := (ODR | bs) & ~br
   }
-  ctrl.onWrite(0x14) { // BRR
+  ctrl.onWrite(0x14) { // 0x14 BRR
     val brrData = io.apb.PWDATA
     ODR := ODR & ~brrData(gpioWidth - 1 downto 0).asUInt
   }
-  // LCKR 锁定寄存器, 永久保留锁定位
-  ctrl.read(LCKR, 0x18) // LCKR
-  ctrl.onWrite(0x18) {
-    val writeVal = io.apb.PWDATA(gpioWidth - 1 downto 0).asUInt
-    // 只对尚未锁定的位设置锁定
-    LCKR := LCKR | (writeVal & ~LCKR)
+  // LCKK+LCKR (bit 16 为 LCKK，0~15 为LCKR)
+  ctrl.read(U(LCKK ## LCKR), 0x18)
+  ctrl.onWrite(0x18) { // 0x18 LCKR
+    val data = io.apb.PWDATA
+    val writeVal = data(15 downto 0).asUInt
+    val writeK = data(16)
+    when(writeK && !LCKK) {
+      LCKK := False
+    }.elsewhen(!writeK && !LCKK) {
+      LCKK := False
+    }.elsewhen(writeK && !LCKK) {
+      LCKK := True
+    }
+    // 仅能设置未锁定位
+    val nextLckr = LCKR | (writeVal & ~LCKR)
+    LCKR := nextLckr
   }
-
-  // // 根据CRL/CRH寄存器简单生成输出使能信号
-  // val gpioDir = Vec(Bool(), gpioWidth)
-  // for (i <- 0 until gpioWidth) {
-  //   val cfg =
-  //     if (i < gpioWidth / 2) CRL(4 * i + 3 downto 4 * i)
-  //     else CRH(4 * (i - gpioWidth / 2) + 3 downto 4 * (i - gpioWidth / 2))
-  //   gpioDir(i) := cfg(1)
-  // }
-
-  // // 三态GPIO口驱动
-  // for (i <- 0 until gpioWidth) {
-  //   io.gpio.write(i) := ODR(i)
-  //   io.gpio.writeEnable(i) := gpioDir(i)
-  // }
+  val LCK = LCKR
 
   // GPIO 模式配置解码
   case class GpioCtrl() extends Bundle {
@@ -101,7 +120,6 @@ case class Apb3Gpio(
     val isOpenDrain = cfg.cnf === 1 || cfg.cnf === 3
     val outputData = Mux(useAfio, io.afio(i), ODR(i))
     val enableOutput = isOutput && (!isOpenDrain || outputData === False)
-
     io.gpio.write(i) := outputData
     io.gpio.writeEnable(i) := enableOutput
   }
